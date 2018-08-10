892d4502fa8b690f5d8ce3df9718f52fc7c78ea6
kitagawa <kitadrum50@gmail.com>
2018-08-10 19:20:18 +0900

    Fix component

--- components/Congratulations.vue
+++ components/Congratulations.vue
@@ -4,14 +4,24 @@ div
     mode="out-in"
     v-on:before-enter="beforeEnter"
     v-on:enter="enter"
+    v-on:after-enter="afterEnter"
     v-on:leave="leave")
     h1.last-text(v-if="show && cong", key="congratulations") Congratulations!!!!
     h1.last-text(v-if="show && !cong", key="play") Let's play again next time!!! 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Homemade+Apple" rel="stylesheet">
 </template>
   
-<script> export default {
+<script>
+export default {
+  head () {
+    return {
+      script: [
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min.js' }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Homemade+Apple' }
+      ]
+    } 
+  },
   props: ["show"],
   data: function() {
     return {
@@ -25,14 +35,21 @@ div
     enter: function (el, done) {
       Velocity(el, { opacity: 1, fontSize: '5.4em' }, { duration: 300 })
       Velocity(el, { fontSize: '5em' }, { complete: done })
-      this.cong = false;
+    },
+    afterEnter: function(el) {
+      if (this.cong) {
+        this.cong = false;
+      } else {
+        console.log("Change Color!");
+        Velocity(el, { color: "#F2B9A1" }, { duration: 300 })
+      }
     },
     leave: function (el, done) {
       Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
-      Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
+      Velocity(el, { rotateZ: '70deg' }, { loop: 2 })
       Velocity(el, {
         rotateZ: '45deg',
-        translateY: '30px',
+        translateY: '1000px',
         translateX: '30px',
         opacity: 0
       }, { complete: done })
@@ -43,6 +60,7 @@ div
 
 <style lang="sass">
 .last-text
+  margin-top: 200px;
   text-align: center;
   font-size: 5em;
   font-family: 'Homemade Apple', cursive; 
